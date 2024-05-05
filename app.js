2c53c6f14777b3ac7f1879d20e4e4a033fadbc8a
M Farhan Hashmi <160288691+123Simplegithub@users.noreply.github.com>
2024-05-06 02:47:07 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,11 @@
+var guess = 300;
+var target = 250;
+if (guess < target) {
+    console.log("Your guess too low");
+}
+else if (guess > target) {
+    console.log("Your guess too high");
+}
+else {
+    console.log("Your guess to correctly!");
+}
---
+++ app.ts
@@ -0,0 +1,9 @@
+let guess: number = 300;
+let target: number = 250;
+if (guess < target) {
+    console.log("Your guess too low");
+} else if (guess > target) {
+ console.log("Your guess too high");
+} else {
+    console.log("Your guess to correctly!");
+}
